798a410731009e6ce20fc01976810304aeebafa0
diogobastos07 <diogobastos01@hotmail.com>
2024-12-16 11:05:24 +0000

    add files to add appointments

---
+++ html/appointmentsAdd.php
@@ -0,0 +1,125 @@
+<?php
+include_once("../html/sidebar.php");
+include_once("../php/db_functions.php");
+?>
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointments</title>
+    <link rel="stylesheet" href="../css/style_menu.css">
+    <link rel="stylesheet" href="../css/appointmentsAdd.css">
+</head>
+
+<body class="main-body">
+    <?php
+        echo '<style>.sidebar .nav_menu .appointments { color: ' . 'white' . '; }</style>';
+    ?>
+    <section class="main-section">
+        <h1>Appointments</h1>
+        <section id="content">    
+            <h2>Add Appointment</h2>
+
+            <form action="../php/appointmentsAdd_functions.php" method="POST">
+
+                <div class="input-box">
+                    <label>Date:</label>
+                    <input id="date_appointment" type="date" name="date_appointment" value="" required>
+                </div>
+
+                <div class="input-box">
+                    <label>Dentist:</label>
+                    <select name="dentist_id">
+                    <?php 
+                    $all_dentists = getAllDentists();
+                    foreach ($all_dentists as $dentist) {
+                        echo '<option value="' . $dentist['id'] . '">' . $dentist['person_name'] . '</option>';
+                    }
+                    ?>
+                    </select>
+                    <p class="warning">
+                    <?php 
+                        if (isset($_GET['message'])) {
+                            $message = urldecode($_GET['message']);
+                            if ($message == 'dentist unavailable') {
+                                echo 'The dentist already has another appointment at that time.';
+                            } elseif ($message == 'dentist not working') {
+                                echo 'The dentist is not working at that time.';
+                            }                            
+                        }
+                    ?></p>
+                </div>
+
+                <div class="input-box">
+                    <label>Patient:</label>
+                    <select name="patient_id">
+                    <?php 
+                    $all_patients = getAllPatients();
+                    foreach ($all_patients as $patient) {
+                        echo '<option value="' . $patient['id'] . '">' . $patient['person_name'] . '</option>';   
+                    }
+                    ?>
+                    </select>
+                    <p class="warning">
+                    <?php 
+                        if (isset($_GET['message'])) {
+                            $message = urldecode($_GET['message']);
+                            if ($message == 'patient unavailable') {
+                                echo 'The patient already has another appointment at that time.';
+                            } 
+                        }
+                    ?></p>
+                </div>
+
+                <div class="input-box">
+                    <label>Assistant:</label>
+                    <select name="assistant_id">
+                    <?php 
+                    $all_assistants = getAllAssistants();
+                    foreach ($all_assistants as $assistant) {
+                        echo '<option value="' . $assistant['id'] . '">' . $assistant['person_name'] . '</option>';   
+                    }
+                    ?>
+                    </select>
+                    <p class="warning">
+                    <?php 
+                        if (isset($_GET['message'])) {
+                            $message = urldecode($_GET['message']);
+                            if ($message == 'assistant unavailable') {
+                                echo 'The assistant already has another appointment at that time.';
+                            } 
+                        }
+                    ?></p>
+                </div>
+
+                <div class="input-box">
+                    <label>Schedule:</label>
+                    <div class="hours" id="hours">
+                        <input type="time" name="start_time" value="' . $start_time . '"required>
+                        <p> - </p>
+                        <input type="time" name="end_time" value="' . $end_time . '"required>
+                    </div>
+                    <p class="warning">
+                    <?php 
+                        if (isset($_GET['message'])) {
+                            $message = urldecode($_GET['message']);
+                            if ($message == 'incorrect schedule') {
+                                echo 'This schedule is not allowed.';
+                            } 
+                        }
+                    ?></p>
+                </div>
+
+                <button type="submit" class="btn" name="cancel" id="btn_cancel" formnovalidate>Return</button>
+                <button type="submit" class="btn" name="add_appointment" id="btn_add">Add</button>
+                </form>
+
+
+        </section>
+    </section>
+
+</body>
+
+</html>
---
+++ php/appointmentsAdd_functions.php
@@ -0,0 +1,68 @@
+<?php 
+include_once("../php/db_functions.php");
+
+if (isset($_POST['add_appointment'])) {
+    $date_appointment = $_POST['date_appointment'];
+    $dentist_id = $_POST['dentist_id'];
+    $patient_id = $_POST['patient_id'];
+    $assistant_id = $_POST['assistant_id'];
+    $start_time = $_POST['start_time'];
+    $end_time = $_POST['end_time'];
+    $appointment_id = NULL;
+
+    $var_den=0;
+    $var_ass=0;
+    $var_pat=0;
+    $var_hour_den=0;
+
+    try {
+        $var_den = availabilityDentist($date_appointment, $dentist_id,  $start_time, $end_time, $appointment_id);
+        $var_ass = availabilityAssistant($date_appointment, $assistant_id, $start_time, $end_time, $appointment_id);
+        $var_pat = availabilityPatient($date_appointment, $patient_id,  $start_time, $end_time, $appointment_id);
+        $var_hour_den = checkIfDentistWorks($dentist_id, $date_appointment, $start_time, $end_time);
+        
+        if($start_time<$end_time) {
+            if ($var_hour_den==0){
+                if ($var_den==0) {
+                    if ($var_pat==0){
+                        if ($var_ass==0) {
+                            addAppointment($date_appointment, $dentist_id, $patient_id, $assistant_id, $start_time, $end_time);
+                            header("Location:../html/appointments.php?date_appointment=".urlencode($date_appointment));
+                            exit();
+                        } else {
+                            $message = 'assistant unavailable';
+                        header("Location: ../html/appointmentsAdd.php?message=".urlencode($message));
+                        exit();
+                        }
+                    } else{
+                        $message = 'patient unavailable';
+                        header("Location: ../html/appointmentsAdd.php?message=".urlencode($message));
+                        exit();
+                    }
+                } else {
+                    $message = 'dentist unavailable';
+                    header("Location: ../html/appointmentsAdd.php?message=".urlencode($message));
+                    exit();
+                }
+            } else {
+                $message = 'dentist not working';
+                header("Location: ../html/appointmentsAdd.php?message=".urlencode($message));
+                exit();
+            }
+        } else {
+            $message = 'incorrect schedule';
+            header("Location: ../html/appointmentsAdd.php?message=".urlencode($message));
+            exit();
+        }
+
+    } catch(PDOException $e) {
+        $err_msg = $e -> getMessage();
+        echo $err_msg;
+        $line = $e -> getLine();
+        echo $line;
+    }
+    
+} elseif (isset($_POST['cancel'])) {
+    header("Location:../html/appointments.php");
+    exit();
+}
